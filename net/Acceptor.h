45db9074b99817359c711f0a4ef4098831a1a11c
Autumn_Luna <691786631@qq.com>
2026-05-30 12:55:59 +0800

    实现的Acceptor类, 用于处理监听套接字的可读事件

---
+++ net/Acceptor.h
@@ -0,0 +1,45 @@
+#ifndef CLEARMOON_NET_ACCEPTOR_H
+#define CLEARMOON_NET_ACCEPTOR_H
+
+#include "../base/noncopy.h"
+#include "InetAddress.h"
+#include "Socket.h"
+#include "EventLoop.h"
+#include "Channel.h"
+
+#include <functional>
+
+namespace clearmoon 
+{
+namespace net 
+{
+
+class Acceptor : public noncopyable
+{
+public:
+using newConnectionCallback = std::function<void(Socket, const InetAddress&)>;
+
+    Acceptor(EventLoop* loop, const InetAddress& addr);
+    ~Acceptor();
+
+    void setConnectionCallback(const newConnectionCallback& cb) { connCallback_ = cb; }
+    //由TcpServer调用
+    void listen();
+
+private:
+    //传给channel的回调
+    void handleRead();
+
+    EventLoop* loop_;
+    Socket listenSock_;
+
+    Channel acceptChannel_;
+
+    newConnectionCallback connCallback_;
+    bool listening_ = false;
+};
+
+}//net
+}//clearmoon
+
+#endif
